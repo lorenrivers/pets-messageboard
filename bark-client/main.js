fa07b6e5a0248219cc0b8c9186c5dc78b8ad3c46
Tomgtech <tom.gendall@yahoo.com>
2024-02-06 12:02:37 +0000

    bark

--- bark-client/main.js
+++ bark-client/main.js
@@ -1,91 +1,115 @@
-const app = document.getElementById('app');
-const createButton = document.querySelector('.create'); 
+const app = document.getElementById("app");
+const createButton = document.querySelector(".create");
 
 const createPostForm = () => {
-
   // create container div
-  let containerDiv = document.createElement('div');
-  containerDiv.setAttribute('class', 'container');
+  let containerDiv = document.createElement("div");
+  containerDiv.setAttribute("class", "container");
   app.appendChild(containerDiv);
 
   // create main post div
-  let submitMessageDiv = document.createElement('div');
-  submitMessageDiv.setAttribute('class', 'submit-message');
+  let submitMessageDiv = document.createElement("div");
+  submitMessageDiv.setAttribute("class", "submit-message");
   containerDiv.appendChild(submitMessageDiv);
 
   // create comments section
-  let commentsDiv = document.createElement('div');
-  commentsDiv.setAttribute('class', 'comments');
+  let commentsDiv = document.createElement("div");
+  commentsDiv.setAttribute("class", "comments");
   containerDiv.appendChild(commentsDiv);
 
   // create form
-  let formOne = document.createElement('form');
-  formOne.setAttribute('class', 'form');
+  let formOne = document.createElement("form");
+  formOne.setAttribute("class", "form");
   submitMessageDiv.appendChild(formOne);
 
   // create username field
-  let username = document.createElement('textarea');
-  username.setAttribute('class', 'username');
-  username.setAttribute('placeholder', 'Enter username');
-  
+  let username = document.createElement("textarea");
+  username.setAttribute("class", "username");
+  username.setAttribute("placeholder", "Enter username");
+
   formOne.appendChild(username);
 
   // create textarea
-  let textarea = document.createElement('textarea');
-  textarea.setAttribute('class', 'post');
-  textarea.setAttribute('placeholder', 'Got something to bark about? Share your woofs here!');
+  let textarea = document.createElement("textarea");
+  textarea.setAttribute("class", "post");
+  textarea.setAttribute(
+    "placeholder",
+    "Got something to bark about? Share your woofs here!"
+  );
   formOne.appendChild(textarea);
 
-  let imgUpload = document.createElement('input');
-  imgUpload.setAttribute('class', 'img-upload');
-  imgUpload.setAttribute('type', 'file');
-
-  formOne.appendChild(imgUpload);
-
-
-
-
   // create submit button
-  let submitButton = document.createElement('button');
-  submitButton.setAttribute('class', 'submit-button');
-  submitButton.textContent = 'Submit';
+  let submitButton = document.createElement("button");
+  submitButton.setAttribute("class", "submit-button");
+  submitButton.textContent = "Submit";
   formOne.appendChild(submitButton);
 
-  // // create comment section
-  // let commentSection = document.createElement('section');
-  // commentSection.setAttribute('class', 'comment-section');
-  // commentsDiv.appendChild(commentSection);
+  submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let newPostDiv = document.createElement("div");
+    let usernameDiv = document.createElement("h3");
+    let postDiv = document.createElement("p");
+    usernameDiv.innerHTML = formOne.querySelector(".username").value;
+    postDiv.innerHTML = formOne.querySelector(".post").value;
+    newPostDiv.appendChild(usernameDiv);
+    newPostDiv.appendChild(postDiv);
+    commentsDiv.appendChild(newPostDiv);
+    formOne.reset();
+    submitMessageDiv.removeChild(formOne);
 
-  // // create comment section header
-  // let commentSectionHeader = document.createElement('h2');
-  // commentSectionHeader.textContent = 'Comments';
-  // commentSection.appendChild(commentSectionHeader);
+    // create comment section
+    let commentSection = document.createElement("section");
+    commentSection.setAttribute("class", "comment-section");
+    commentsDiv.appendChild(commentSection);
 
-  // // create comment section list
-  // let commentSectionList = document.createElement('ul');
-  // commentSectionList.setAttribute('class', 'comment-section-list');
-  // commentSection.appendChild(commentSectionList);
+    // create comment section header
+    let commentSectionHeader = document.createElement("h2");
+    commentSectionHeader.textContent = "Comments";
+    commentSection.appendChild(commentSectionHeader);
 
-  // // create comment section form
-  // let commentSectionForm = document.createElement('form');
-  // commentSectionForm.setAttribute('class', 'comment-section-form');
-  // commentSection.appendChild(commentSectionForm);
+    // create comment section list
+    let commentSectionList = document.createElement("ul");
+    commentSectionList.setAttribute("class", "comment-section-list");
+    commentSection.appendChild(commentSectionList);
 
-  // // create comment section textarea
-  // let commentSectionTextarea = document.createElement('textarea');
-  // textarea.setAttribute('class', 'textarea');
-  // commentSectionForm.appendChild(commentSectionTextarea);
+    // create comment section form
+    let commentSectionForm = document.createElement("form");
+    commentSectionForm.setAttribute("class", "comment-section-form");
+    commentSection.appendChild(commentSectionForm);
 
-  // let commentSectionUsername = document.createElement('textarea');
-  // textarea.setAttribute('class', 'username');
-  // commentSectionForm.appendChild(commentSectionUsername);
+    // create comment section textarea
+    let commentSectionTextarea = document.createElement("textarea");
+    commentSectionTextarea.setAttribute("class", "textarea");
+    commentSectionForm.appendChild(commentSectionTextarea);
 
-  // // create comments section submit button
-  // let commentSectionSubmitButton = document.createElement('button');
-  // commentSectionSubmitButton.setAttribute('class', 'comment-section-submit-button');
-  // commentSectionSubmitButton.textContent = 'Submit';
-  // commentSectionForm.appendChild(commentSectionSubmitButton);
+    let commentSectionUsername = document.createElement("textarea");
+    commentSectionUsername.setAttribute("class", "username");
+    commentSectionUsername.setAttribute("placeholder", "username");
+    commentSectionForm.appendChild(commentSectionUsername);
 
-}
+    // create comments section submit button
+    let commentSectionSubmitButton = document.createElement("button");
+    commentSectionSubmitButton.setAttribute(
+      "class",
+      "comment-section-submit-button"
+    );
+    commentSectionSubmitButton.textContent = "Submit";
+    commentSectionForm.appendChild(commentSectionSubmitButton);
 
-createButton.addEventListener('click', createPostForm);
+    commentSectionSubmitButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      let newCommentDiv = commentsDiv.createElement("div");
+      let commentUsername = commentsDiv.createElement("h3");
+      let commentText = commentsDiv.createElement("p");
+      commentUsername.innerHTML =
+        commentSectionForm.querySelector(".username").value;
+      commentText.innerHTML =
+        commentSectionForm.querySelector(".textarea").value;
+      newCommentDiv.appendChild(commentUsername);
+      newCommentDiv.appendChild(commentText);
+      commentSectionForm.reset();
+    });
+  });
+};
+
+createButton.addEventListener("click", createPostForm);
